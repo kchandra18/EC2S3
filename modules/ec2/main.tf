df1791fa35abf7adb135bbec85e96ad0b8b30910
kchandra18 <chandrasekhar75168@gmail.com>
2026-04-21 09:32:44 +0530

    fix duplicate EC2 resource issue

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -11,39 +11,10 @@ data "aws_subnets" "default" {
   }
 }
 
-# EC2 Instance
+# EC2 Instance (ONLY ONE BLOCK)
 resource "aws_instance" "ec2" {
   ami           = var.ami_id
-
-  # 🔥 FORCE FREE TIER (FIX)
-  instance_type = "t2.micro"
-
-  subnet_id = data.aws_subnets.default.ids[0]
-
-  tags = {
-    Name        = "${var.project_name}-${var.environment}-ec2"
-    Environment = var.environment
-    Project     = var.project_name
-  }
-}# Get default VPC
-data "aws_vpc" "default" {
-  default = true
-}
-
-# Get subnets
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-# EC2 Instance
-resource "aws_instance" "ec2" {
-  ami           = var.ami_id
-
-  # 🔥 FORCE FREE TIER (FIX)
-  instance_type = "t2.micro"
+  instance_type = "t2.micro"   # free tier safe
 
   subnet_id = data.aws_subnets.default.ids[0]
 
