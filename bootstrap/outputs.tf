95c073d99efd7664d0e7b19bafc91d27f1111b07
kchandra18 <chandrasekhar75168@gmail.com>
2026-04-21 08:42:45 +0530

    Switch bootstrap to use existing resources via data sources

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -1,7 +1,9 @@
 output "bucket_name" {
-  value = aws_s3_bucket.tf_state.bucket
+  description = "Existing S3 bucket used for Terraform state"
+  value       = data.aws_s3_bucket.tf_state.bucket
 }
 
 output "dynamodb_table" {
-  value = aws_dynamodb_table.tf_lock.name
+  description = "Existing DynamoDB table used for locking"
+  value       = data.aws_dynamodb_table.tf_lock.name
 }
