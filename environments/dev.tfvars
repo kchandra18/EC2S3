9bcf7c758ed116de3b761c0ba92f7495a759fa34
kchandra18 <chandrasekhar75168@gmail.com>
2026-04-21 09:36:40 +0530

    final working ec2 deployment using t3.micro

--- environments/dev.tfvars
+++ environments/dev.tfvars
@@ -2,7 +2,9 @@ aws_region   = "us-east-1"
 environment  = "dev"
 project_name = "ec2s3-project"
 
-instance_type = "t2.micro"
-ami_id        = "ami-0c02fb55956c7d316"
+# (ignored now but kept for structure)
+instance_type = "t3.micro"
+
+ami_id = "ami-0c02fb55956c7d316"
 
 s3_bucket_name = "ec2s3-dev-dinesh-001"
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -11,10 +11,12 @@ data "aws_subnets" "default" {
   }
 }
 
-# EC2 Instance (ONLY ONE BLOCK)
+# EC2 Instance
 resource "aws_instance" "ec2" {
   ami           = var.ami_id
-  instance_type = "t2.micro"   # free tier safe
+
+  # ✅ WORKS IN ALL ACCOUNTS
+  instance_type = "t3.micro"
 
   subnet_id = data.aws_subnets.default.ids[0]
 
