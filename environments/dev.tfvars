28367d13372938469eeb2ffa08b673f861b7f920
kchandra18 <chandrasekhar75168@gmail.com>
2026-04-21 09:31:04 +0530

    final fix ec2 free tier deployment

--- environments/dev.tfvars
+++ environments/dev.tfvars
@@ -2,9 +2,7 @@ aws_region   = "us-east-1"
 environment  = "dev"
 project_name = "ec2s3-project"
 
-# EC2 (FREE TIER SAFE)
 instance_type = "t2.micro"
 ami_id        = "ami-0c02fb55956c7d316"
 
-# S3 (APP BUCKET - UNIQUE)
 s3_bucket_name = "ec2s3-dev-dinesh-001"
--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -14,7 +14,36 @@ data "aws_subnets" "default" {
 # EC2 Instance
 resource "aws_instance" "ec2" {
   ami           = var.ami_id
-  instance_type = var.instance_type
+
+  # 🔥 FORCE FREE TIER (FIX)
+  instance_type = "t2.micro"
+
+  subnet_id = data.aws_subnets.default.ids[0]
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-ec2"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get subnets
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "ec2" {
+  ami           = var.ami_id
+
+  # 🔥 FORCE FREE TIER (FIX)
+  instance_type = "t2.micro"
 
   subnet_id = data.aws_subnets.default.ids[0]
 
