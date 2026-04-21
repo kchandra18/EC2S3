146865c066603428e6ebc80d5cd4dafec9a570f0
kchandra18 <chandrasekhar75168@gmail.com>
2026-04-21 08:51:58 +0530

    Configure remote backend using S3 and DynamoDB

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ec2s3-terraform-state-dinesh-001"  # CHANGE THIS UNIQUE
+    bucket         = "ec2s3-terraform-state-dinesh-001"
     key            = "ec2s3/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "ec2s3-terraform-lock"
