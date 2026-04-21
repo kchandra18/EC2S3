d4012969c0b532fda8bf7975708290f824bfeecf
kchandra18 <chandrasekhar75168@gmail.com>
2026-04-21 10:38:02 +0530

    configure separate terraform state per environment

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "ec2s3-terraform-state-dinesh-001"
-    key            = "ec2s3/terraform.tfstate"
+    key            = "env/${var.environment}/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "ec2s3-terraform-lock"
     encrypt        = true
