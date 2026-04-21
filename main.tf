5561b7814c14f1ab170bc94d77b10eece2c5bc92
kchandra18 <chandrasekhar75168@gmail.com>
2026-04-21 09:08:15 +0530

    Fix pipeline to run main Terraform instead of bootstrap

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+module "ec2" {
+  source = "./modules/ec2"
+
+  ami_id        = var.ami_id
+  instance_type = var.instance_type
+  environment   = var.environment
+  project_name  = var.project_name
+}
+
 module "s3" {
   source = "./modules/s3"
 
