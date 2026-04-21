818272a9417c3e08fc1179b121c8723c8b2ed422
kchandra18 <chandrasekhar75168@gmail.com>
2026-04-21 10:41:12 +0530

    final pipeline with env-based backend and full devsecops

--- backend.tf
+++ backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
     bucket         = "ec2s3-terraform-state-dinesh-001"
-    key            = "env/${var.environment}/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "ec2s3-terraform-lock"
     encrypt        = true
